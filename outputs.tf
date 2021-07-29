342aafa516b8bdf8abaa8f1243c856f99e499955
Dan <me.daniel.meier@gmail.com>
2021-07-29 14:53:58 +0100

    change outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "s3_bucket_id" {
-  description = "The name of the bucket."
-  value       = element(concat(aws_s3_bucket_policy.this.*.id, aws_s3_bucket.this.*.id, [""]), 0)
-}
-
 output "s3_bucket_arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
   value       = element(concat(aws_s3_bucket.this.*.arn, [""]), 0)
