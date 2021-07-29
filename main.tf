a164c4b0aa993363087e5d939ed6c74eb4454f29
Dan <me.daniel.meier@gmail.com>
2021-07-29 19:49:21 +0100

    add aws_s3_bucket_public_access_block

--- main.tf
+++ main.tf
@@ -30,6 +30,14 @@ resource "aws_s3_bucket" "this" {
 	}
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  count                   = "${length(var.bucketnames)}"
+  bucket                  = "${element(var.bucketnames, count.index)}"
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
 # generate folderstructure in every bucket
 resource "aws_s3_bucket_object" "folders" {
 
